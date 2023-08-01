6082818e947f6878a4c25fba2266525097d17780
Ubuntu <ubuntu@ip-172-31-20-13.eu-north-1.compute.internal>
2023-08-01 19:25:27 +0000

    check

--- form_project/settings.py
+++ form_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ekj6uq@^r@x#o_toeuh!*#h!$$ust&75y$t4n@6w^a153kmala
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["core-form.herokuapp.com","*","http://127.0.0.1:8000/","https://virsa.azurewebsites.net"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
